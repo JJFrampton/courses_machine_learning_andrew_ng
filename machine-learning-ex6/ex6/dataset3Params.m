f53e018f4e5beb72a9ec9825cfc6188f9188e2f2
Joseph Frampton <josephjamesframpton@gmail.com>
2019-01-02 17:43:12 -0700

    finished ex6
    JJF

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,7 +23,32 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+values = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+err_min = 1;
 
+for i=1:8
+
+  for n=1:8
+
+    C_2 = values(i);
+    sigma_2 = values(n);
+    fprintf('Testing :\nC : %f\nsigma : %f\n', C_2, sigma_2)
+    % run the function
+    model = svmTrain(X, y, C_2, @(x1, x2) gaussianKernel(x1, x2, sigma_2));
+    predictions = svmPredict(model, Xval);
+    err = mean(double(predictions ~= yval));
+    if err < err_min
+      fprintf('New min found at : \nError: %f\nC : %f\nsigma : %f\n', err_min, C, sigma)
+      err_min = err;
+      C = C_2;
+      sigma = sigma_2;
+    end
+    
+    n = n + 1;
+  end
+
+  i = i + 1;
+end
 
 
 
